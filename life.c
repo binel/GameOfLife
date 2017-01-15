77ccc8ea8eaeb2dd37668025c3af96a40bc1325d
Ben Nelson <binelson@iastate.edu>
2017-01-15 17:18:45 -0600

    Actually simulates and basic blinker works, but the test inputs don't produce expected behavior

--- life.c
+++ life.c
@@ -10,15 +10,24 @@ void printGrid(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum);
 
 int checkAlive(char grid[GRIDHEIGHT][GRIDWIDTH], int x, int y);
 
-int simulate(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum);
+void  simulate(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum);
+
+int countNeighbors(char grid[GRIDHEIGHT][GRIDWIDTH], int x, int y); 
 
 int main(int argc, char* argv[]) {
-  
 
   char grid[GRIDHEIGHT][GRIDWIDTH];
   int frameNum = 0; 
+
+  //initalize the grid to all spaces 
+  int i, j; 
+
+  for(i = 0; i < GRIDHEIGHT; i++){
+    for(j = 0; j < GRIDWIDTH; j++){ 
+      grid[i][j] = ' ';
+    }
+  }
   //populate the grid using command args
-  int i; 
   for(i = 1; i < argc; i = i + 2) {
     int x = atoi(argv[i]);
     int y = atoi(argv[i + 1]);
@@ -26,79 +35,103 @@ int main(int argc, char* argv[]) {
   } 
 
   printGrid(grid, frameNum);
+  usleep(83333);
   while(TRUE) {
-    frameNum = simulate(grid, frameNum);
+    frameNum++; 
+    simulate(grid, frameNum);
   }
 }
 
-int simulate(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum) {
-    //Simulate 
-    char nextGrid[GRIDHEIGHT][GRIDWIDTH]; 
-    int x, y; 
-    for(y = 0; y < GRIDHEIGHT; y++) {
-      for(x = 0; x < GRIDWIDTH; x++) {
-	//what is the status of this cell
-	int isAlive = aliveCheck(grid, x, y);
-
-	//count the number of neighbors of this cell 
-	int numNeighbors = 0; 
-
-	//count in circle 
-	int i, j; 
-	for(i = x - 1; i < x + 1; i++) {
-	  for(j = y - 1; j < y + 1; j++) {
-	    //handles wrapping
-	    if(i < 0) {
-	      i = GRIDWIDTH - 1;  
-	    }
-	    if(i > GRIDWIDTH) {
-	      i = 0; 
-	    }
-	    if(j < 0) { 
-	      j = GRIDHEIGHT - 1; 
-	    }
-	    if(j > GRIDHEIGHT) {
-	      j = 0; 
-	    }
-
-	    numNeighbors += aliveCheck(grid, i, j); 
-
-	    //determines if alive in the next round
-	    //1. If the cell is alive and has 0 or 1 live neighbors, it dies 
-	    if(isAlive == TRUE && numNeighbors < 2) {
-	      isAlive = FALSE; 
-	    }
-	    //2. If the cell is alive and has 2 or 3 live neighbors, it lives 
-	    else if(isAlive == TRUE && numNeighbors < 4) {
-	      isAlive = TRUE;
-	    }
-	    //3. If the cell is alive and has 4 live neighbors, it dies 
-	    else if(isAlive == TRUE && numNeighbors == 4) {
-	      isAlive = FALSE; 
-	    }
-	    //4. If the cell is dead and has exactly 3 live neighbors, it lives
-	    else if(isAlive == FALSE && numNeighbors == 3) {
-	      isAlive = TRUE; 
-	    }
-          
-	    //add cell to next grid 
-	    if(isAlive == TRUE) {
-	      nextGrid[y][x] = 'O'; 
-	    }
-	  }
-	}
-      } 
+int countNeighbors(char grid[GRIDHEIGHT][GRIDWIDTH], int x, int y) {
+  int numNeighbors = 0;
+  int i, j; 
+  for(i = x - 1; i <= x + 1; i++) {
+    for(j = y - 1; j <= y + 1; j++) {
+      int xcheck = i; 
+      int ycheck = j; 
+      //handles wrapping 
+      if(i < 0) {
+	xcheck = GRIDWIDTH - 1;  
+      }
+      if(i >= GRIDWIDTH) {
+	xcheck = 0; 
+      }
+      if(j < 0) { 
+	ycheck = GRIDHEIGHT - 1; 
+      }
+      if(j >= GRIDHEIGHT) {
+	ycheck = 0; 
+      }
+      if(!(i == x && j == y)) {
+	numNeighbors += aliveCheck(grid, xcheck, ycheck); 
+      }
     }
-    grid = nextGrid; 
-    frameNum++;
-    printGrid(grid, frameNum); 
-    usleep(1000000);
-    return frameNum; 
+  }
+  return numNeighbors; 
+}
+
+
+void simulate(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum) {
+  //Simulate 
+  char nextGrid[GRIDHEIGHT][GRIDWIDTH];
+  int i, j; 
+  //initalize the grid 
+  for(i = 0; i < GRIDHEIGHT; i++){
+    for(j = 0; j < GRIDWIDTH; j++){ 
+      nextGrid[i][j] = ' ';
+    }
+  }
+  //determine status of each cell 
+  int x, y; 
+  for(y = 0; y < GRIDHEIGHT; y++) {
+    for(x = 0; x < GRIDWIDTH; x++) {
+      //what is the status of this cell
+      int isAlive = aliveCheck(grid, x, y);
+
+      //count the number of neighbors of this cell 
+      int numNeighbors = countNeighbors(grid, x, y);
+      printf("%d", numNeighbors); 
+      //determines if alive in the next round
+      //1. If the cell is alive and has 0 or 1 live neighbors, it dies 
+      if(isAlive == TRUE && numNeighbors < 2) {
+	isAlive = FALSE; 
+      }
+      //2. If the cell is alive and has 2 or 3 live neighbors, it lives 
+      else if(isAlive == TRUE && numNeighbors < 4) {
+	isAlive = TRUE;
+      }
+      //3. If the cell is alive and has 4 live neighbors, it dies 
+      else if(isAlive == TRUE && numNeighbors == 4) {
+	isAlive = FALSE; 
+      }
+      //4. If the cell is dead and has exactly 3 live neighbors, it lives
+      else if(isAlive == FALSE && numNeighbors == 3) {
+	isAlive = TRUE; 
+      }
+      
+      //add cell to next grid 
+      if(isAlive == TRUE) {
+	nextGrid[y][x] = 'O'; 
+      } else {
+	nextGrid[y][x] = ' ';
+      }
+    } 
+    printf("\n");
+  }
+  //copy old grid to new grid
+  for(i = 0; i < GRIDHEIGHT; i++){
+    for(j = 0; j < GRIDWIDTH; j++){ 
+      grid[i][j] = nextGrid[i][j];
+    }
+  }
+  printGrid(grid, frameNum); 
+  usleep(83333);
+  return; 
 }
 
 int aliveCheck(char grid[GRIDHEIGHT][GRIDWIDTH], int x, int y) {
   int isAlive = FALSE; 
-    if(grid[y][x] == 'O') {
+  if(grid[y][x] == 'O') {
       isAlive = TRUE; 
   }
   return isAlive; 
@@ -109,13 +142,9 @@ void printGrid(char grid[GRIDHEIGHT][GRIDWIDTH], int frameNum){
 
   for(i = 0; i < GRIDHEIGHT; i++) {
     for(j = 0; j < GRIDWIDTH; j++) {
-      if(grid[i][j] != 'O'){
-        printf(" ");
-      } else {
-        printf("O");
-      } 
+      printf("%c", grid[i][j]);
     }
     printf("\n");
   }
-  printf("FRAME %i", frameNum); 
+  printf("FRAME %i\n", frameNum); 
 }
